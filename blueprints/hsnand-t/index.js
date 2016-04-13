92390a5ec7e0bf08f644811a482e7eca339e2879
Brandon Drake <brandon@eventboard.io>
2016-04-13 06:55:15 -0600

    Added blueprint

---
+++ blueprints/hsnand-t/index.js
@@ -0,0 +1,19 @@
+/*jshint node:true*/
+module.exports = {
+  normalizeEntityName: function() {},
+  afterInstall: function() {
+    /*
+    this.insertIntoFile('.jshintrc', '    "BowerPackage",', {
+      after: '"predef": [\n'
+    });
+
+    this.insertIntoFile('tests/.jshintrc', '    "BowerPackage",', {
+      after: '"predef": [\n'
+    });
+
+    return this.addBowerPackagesToProject([
+      //{ name: 'git@github.com:tomoguisuru/BowerRepo.git', target: '1.0.1' }
+    ]);
+    */
+  }
+};
